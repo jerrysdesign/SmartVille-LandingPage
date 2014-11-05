6be6311bd9b42e24efeb594eef2f139b3984c323
cooperchao0320@gmai.com <cooperchao0320@gmai.com>
2014-11-05 16:49:48 +0800

    整理 script 檔。

--- templates/project/source/js/script.js
+++ templates/project/source/js/script.js
@@ -1,62 +1,32 @@
-// $('html').css('overflow', 'auto');
-// $('body').css('overflow-x', 'hidden');
+/*	----------------------------------------
+*	Global
+*	----------------------------------------
+*/	
+	var $win = $(window),
+		$section = $('.section'),
+		$menu = $('#menu_top'),
+		$menuTop = $menu.offset().top,
+		$hpGroup = $('.hp-group'),
+		$pad = $('.pad'),
+		$previewBlock = $('.preview-block'),
+		winWidth = $win.width(),
+		winScrollTop = $win.scrollTop(),
+		loading_timer ,
+		loading_count = 0,
+		_a = 0;
 
-// if (typeof console == "undefined") var console = { log: function() {} };
+/*	----------------------------------------
+*	Function
+*	----------------------------------------
+*	00 - Loading
+*	01 - Slide_Mask
+*	02 - Banner1_Area
+*	03 - Tag_Tab
+*	04 - Situation_svg
+*	----------------------------------------
+*/
 
-// var $section = $('section'),
-// 	si = $section.index();
-// 	console.log(si);
-
-// for(i=0; i<=si; i++) {
-// 	console.log($section.index(i).offset().top);
-// }
-
-
-var $win = $(window),
-	$s0 = $('#section-0'),
-	$s1 = $('#section-1'),
-	$s2 = $('#section-2'),
-	$s3 = $('#section-3'),
-	$s4 = $('#section-4'),
-	$s5 = $('#section-5'),
-	$s6 = $('#section-6'),
-
-	$s0Top = $s0.offset().top,
-	$s1Top = $s1.offset().top,
-	$s2Top = $s2.offset().top,
-	$s3Top = $s3.offset().top,
-	$s4Top = $s4.offset().top,
-	$s5Top = $s5.offset().top,
-	$s6Top = $s6.offset().top,
-
-	$menu = $('#menu_top'),
-	$menuTop = $menu.offset().top,
-
-	$hpGroup = $('.hp-group'),
-	$pad = $('.pad'),
-	$previewBlock = $('.preview-block'),
-	
-	winWidth = $win.width(),
-	winScrollTop = $win.scrollTop();
-
-var loading_timer ;
-var loading_count = 0;
-
-// 關閉loading  開啟換盤動畫
-$win.load(function(){
-	clearInterval(loading_timer);
-	
-	if(loading_count<100){
-		for(var i=loading_count;i<=100;i++){
-			$("#loading_progress").html("Please wait..  "+i + "%");
-		}
-	}
-	$('#loading_box').fadeOut(1000);
-});
-
-$(function(){
-	//loading 
-	loading_timer = setInterval(loading_progress, 5);
+	//	00
 	function loading_progress(){
 		loading_count++;
 		if(loading_count>100){
@@ -65,37 +35,126 @@ $(function(){
 		}
 		$("#loading_progress").html("Please wait..  "+loading_count + "%");
 	}
-});
 
+	//	01
+	function slide_mask(idx,old){
+		var _wrap = $('.slide-mask-wrapper'),
+			_scbg = $('.screen-bg'),
+			_mask = _wrap.children(),
+			_spd  = 1500,
+			_dfh  = 620;
+		//	Mask
+		_wrap.stop().animate({'height':_dfh}, 0).delay(_spd).animate({'height':0}, 0);
+		_mask.stop().animate({'top':_dfh}, _spd).animate({'top': -_dfh}, 0);
+		_scbg
+		.children().css('z-index', 0)
+		.eq(old).css('z-index', 3).end()
+		//	pad screenshot
+		.eq(idx).css({'z-index': 4,'opacity':'0'}).stop().delay(_spd/1.5).animate({'opacity':'1'}, _spd/2.5);
+	}
+
+	//	02
+	function banner1_area(obj){
+	    var _old = $('.screen-bg').attr('data-pic');
+
+	    if(obj != _old){
+		    switch (obj)
+		    {
+		        case 0:
+		            $('.screen-bg')
+		            .children().css('z-index', 0)
+		            .eq(_old).css('z-index', 3).end()
+		            .eq(0).css({'z-index': 4,'opacity':'0'}).stop().animate({'opacity':'1'}, 500);
+		            $('.screen-bg').attr('data-pic',0);
+		        break;
+		        case 1:
+		            $('.screen-bg')
+		            .children().css('z-index', 0)
+		            .eq(_old).css('z-index', 3).end()
+		            .eq(1).css({'z-index': 4,'opacity':'0'}).stop().animate({'opacity':'1'}, 500);
+		            $('.screen-bg').attr('data-pic',1);
+		        break;
+		        case 2:
+		            $('.screen-bg')
+		            .children().css('z-index', 0)
+		            .eq(_old).css('z-index', 3).end()
+		            .eq(2).css({'z-index': 4,'opacity':'0'}).stop().animate({'opacity':'1'}, 500);
+		            $('.screen-bg').attr('data-pic',2);
+		        break;
+		        case 3:
+		            $('.screen-bg')
+		            .children().css('z-index', 0)
+		            .eq(_old).css('z-index', 3).end()
+		            .eq(3).css({'z-index': 4,'opacity':'0'}).stop().animate({'opacity':'1'}, 500);
+		            $('.screen-bg').attr('data-pic',3);
+		        break;
+		    }
+	    }
+	}
+
+	//	03
+	function tag_tab(obj){
+		var _this = obj,
+			_cont = $('.tag-tab-cont').children(),
+			_wrap = $('#situation_svg'),
+			_mask = _wrap.find('.mask'),
+			_idx  = obj.index();
+		_this.addClass('active').siblings().removeClass();
+		_cont.eq(_idx).show().siblings().hide();
+		_mask.hide().eq(_idx).show();
+	}
+
+	//	04
+	function situation_svg(){
+		var _wrap = $('#situation_svg'),
+			_mask = _wrap.find('.mask'),
+			_link = _wrap.find('.link'),
+			_tips = $('.tag-tab-cont').children('li'),
+			_tabs = $('.tag-tab').find('li'),
+			_atv  = 'active';
+
+		_link.mouseenter(function(){
+			var _this = $(this),
+				_idx  = _this.index();
+			_mask.hide().eq(_idx).show();
+			_tips.hide().eq(_idx).show();
+			_tabs.removeClass(_atv).eq(_idx).addClass(_atv);
+		});
+	}
 
 $(function(){
 
-
-	var slider = $('.bxslider').bxSlider({
-		mode: 'fade',
-		captions: true,
-		// pagerCustom: '.bx-pager',
-		onSlideAfter: function(){
-		// do mind-blowing JS stuff here
+	// Close loading / Open Translation-effect
+	$win.load(function(){
+		clearInterval(loading_timer);
+		if(loading_count<100){
+			for(var i=loading_count;i<=100;i++){
+				$("#loading_progress").html("Please wait..  "+i + "%");
+			}
 		}
+		$('#loading_box').fadeOut(1000);
 	});
 
-	$win.scroll(function(e) {
-		if ( winScrollTop = 508 ) {
-			e.preventDefault();
-			// slider.reloadSlider();
-		}
-	}).scroll();
+	// Loading 
+	loading_timer = setInterval(loading_progress, 5);
 
+	// BxSlider API Setting
+	var slider = $('.bxslider').bxSlider({
+		mode: 'fade',
+		captions: true
+	});
+
+	// Window Scroll
 	$win.scroll(function() {
-		var winScrollTop = $win.scrollTop();
-		var margin = 0,
+		var winScrollTop = $win.scrollTop(),
+			margin = 0,
+			$s2Top = $('.section').eq(1).offset().top,
 			topOfWindow = winScrollTop + margin;
-		
+
 		if ($s2Top > topOfWindow) {
 			$hpGroup.removeClass().addClass("hp-group");
 		}
-		// senction 0 時
+		// senction = 0
 		if ( winScrollTop <= 206 ) {
 			$('.hand--l, .hand--r').removeClass("hide");
 		}
@@ -103,7 +162,6 @@ $(function(){
 		if ( winScrollTop > 207 ){
 			$('.hand--l, .hand--r').addClass("hide");
 		}
-		
 		if (winScrollTop < 508) {
 			$hpGroup.removeClass("transform");
 			$hpGroup.removeClass("gogo");
@@ -124,110 +182,19 @@ $(function(){
 		if ($s2Top-200 < topOfWindow) {
 			$previewBlock.delay(500).addClass('show');
 		}
-		
 		if ( winScrollTop < 500 ) {
 			$('.text-a,.text-b,.text-c,.text-d').fadeIn(0);
-
-			// #menu_top 的背景透明度變化
+			// #menu_top - background opacity
 			$menu.css('background-color', 'rgba(255,255,255,' + .25 + ')');
 		}
 		if ( winScrollTop > 500 ){
 			$('.text-a,.text-b,.text-c,.text-d').fadeOut(0);
-
-			// #menu_top 的背景透明度變化
+			// #menu_top - background opacity
 			$menu.css('background-color', 'rgba(255,255,255,' + 1 + ')');
 		}
-
 	}).scroll();
-});
 
-function slide_mask(idx,old){
-	var _wrap = $('.slide-mask-wrapper'),
-		_scbg = $('.screen-bg'),
-		_mask = _wrap.children(),
-		_spd  = 1500,
-		_dfh  = 620;
-	// 遮罩
-	_wrap.stop().animate({'height':_dfh}, 0).delay(_spd).animate({'height':0}, 0);
-	_mask.stop().animate({'top':_dfh}, _spd).animate({'top': -_dfh}, 0);
-	_scbg
-	.children().css('z-index', 0)
-	.eq(old).css('z-index', 3).end()
-	// pad screenshot
-	.eq(idx).css({'z-index': 4,'opacity':'0'}).stop().delay(_spd/1.5).animate({'opacity':'1'}, _spd/2.5);
-}
-
-function tag_tab(obj){
-	var _this = obj,
-		_cont = $('.tag-tab-cont').children(),
-		_wrap = $('#situation_svg'),
-		_mask = _wrap.find('.mask'),
-		_idx  = obj.index();
-	_this.addClass('active').siblings().removeClass();
-	_cont.eq(_idx).show().siblings().hide();
-	_mask.hide().eq(_idx).show();
-}
-
-function banner1_area(obj){
-    var _old = $('.screen-bg').attr('data-pic');
-
-    if(obj != _old){
-	    switch (obj)
-	    {
-	        case 0:
-	            $('.screen-bg')
-	            .children().css('z-index', 0)
-	            .eq(_old).css('z-index', 3).end()
-	            .eq(0).css({'z-index': 4,'opacity':'0'}).stop().animate({'opacity':'1'}, 500);
-	            $('.screen-bg').attr('data-pic',0);
-	        break;
-	        case 1:
-	            $('.screen-bg')
-	            .children().css('z-index', 0)
-	            .eq(_old).css('z-index', 3).end()
-	            .eq(1).css({'z-index': 4,'opacity':'0'}).stop().animate({'opacity':'1'}, 500);
-	            $('.screen-bg').attr('data-pic',1);
-	        break;
-	        case 2:
-	            $('.screen-bg')
-	            .children().css('z-index', 0)
-	            .eq(_old).css('z-index', 3).end()
-	            .eq(2).css({'z-index': 4,'opacity':'0'}).stop().animate({'opacity':'1'}, 500);
-	            $('.screen-bg').attr('data-pic',2);
-	        break;
-	        case 3:
-	            $('.screen-bg')
-	            .children().css('z-index', 0)
-	            .eq(_old).css('z-index', 3).end()
-	            .eq(3).css({'z-index': 4,'opacity':'0'}).stop().animate({'opacity':'1'}, 500);
-	            $('.screen-bg').attr('data-pic',3);
-	        break;
-	    }
-    }
-}
-
-// situation_svg
-function situation_svg(){
-	var _wrap = $('#situation_svg'),
-		_mask = _wrap.find('.mask'),
-		_link = _wrap.find('.link'),
-		_tips = $('.tag-tab-cont').children('li'),
-		_tabs = $('.tag-tab').find('li'),
-		_atv  = 'active';
-
-	_link.mouseenter(function(){
-		var _this = $(this),
-			_idx  = _this.index();
-		_mask.hide().eq(_idx).show();
-		_tips.hide().eq(_idx).show();
-		_tabs.removeClass(_atv).eq(_idx).addClass(_atv);
-	});
-
-}
-
-
-$(function(){
-	// menu-area
+	// Menu-Area
 	$('.screen-bg > div:eq(0)').css('z-index', 4);
 	$('#slide-btn li').click(function(){
 		var _this = $(this),
@@ -243,11 +210,11 @@ $(function(){
 				$('#mian-slider > li').eq(_this.index()).show().siblings().hide();
 			}, 400);
 		}
-		// pages btn 
+		// service - pages_btn 
 		$('.bx-pager-item').eq(_this.index()).children().click();
 	});
 
-	// pages
+	// Service-Pages
 	$('body').on('click','.bx-pager-item a',function(){
 		var _idx = $(this).parent().index();
 		banner1_area(_idx);
@@ -258,7 +225,7 @@ $(function(){
 		}, 400);		
 	});
 	
-	// prev & next
+	// Prev & Next
 	$('body').on('click','.bx-controls-direction a',function(){
 		var _idx = $('.bx-pager-item .active').parent().index();
 		$('.bx-pager-item a').eq(_idx).click();
@@ -268,42 +235,35 @@ $(function(){
 		}, 400);
 	});
 
-	// tag-tabs
+	// Tag-Tabs
 	$('.tag-tab li').mouseenter(function(){
 		tag_tab($(this));
 	});
 	$('.tag-tab-cont').children().not(':eq(0)').hide();
 
-	// situation_svg
+	// Situation_svg
 	situation_svg();
-});
-
-
-$(function(){
 	
-	var $win = $(window);
-
 	// Scroll up bar
 	$menu.scrollupbar();
-	// 滾動視差
+
+	// Parallax Scrolling
 	$.stellar({
 		horizontalScrolling: false,
 		verticalOffset: 0
 	});
 
-	// 滾動動畫
+	// WOW Scrolling - effect
 	var ww = $win.width();
 	if(ww > 480){
 		wow = new WOW({
-		animateClass: 'animated',
-		offset: 0
+			animateClass: 'animated',
+			offset: 0
 		});
 		wow.init();
 	}
 
-	// 捲動換單元
-
-	// menu-top
+	// Menu-top
 	var $topLinks = $('.navigation-top').find('li'),
 		$rightLinks = $('.navigation-right').find('li'),
 		$Links = $('.navigation-top,.navigation-right').find('li'),
@@ -316,16 +276,14 @@ $(function(){
 			$rightLinksDatasection = $('.navigation-right li[data-section="' + datasection + '"]'),
 			$sectionDatasection = $('.section[data-section="' + datasection + '"]');
 
-		// scrool 增減樣式
+		// Scrolling - Toggle_style
 		$Links.removeClass('active');
 		$section.removeClass('current');
 		$topLinksDatasection.addClass('active');
 		$rightLinksDatasection.addClass('active');
 		$sectionDatasection.addClass('current');
-
 		// console.log('datasection'+':'+datasection);
 	});
-
 	$win.scroll(function(){
 		if ($win.scrollTop() == 0){
 			$Links.removeClass('active');
@@ -333,18 +291,9 @@ $(function(){
 		}
 	}).scroll();
 
+	// GoToByScroll
 	function goToByScroll(datasection){
 		var goal = $('.section[data-section="' + datasection + '"]').offset().top;
-		// console.log("goal"+"="+ goal);
-
-		// if ($win.scrollTop() < goal){
-		// 	var goalPx = goal + 50;
-		// 	console.log("goalPxif"+"="+ goalPx);
-		// }
-		// else{
-		// 	var goalPx = goal - 50;
-		// 	console.log("goalPxelse"+"="+ goalPx);
-		// }
 
 		function offSetKey1(){
 			if ($win.scrollTop() < goal){
@@ -373,22 +322,22 @@ $(function(){
 			}, 1500, 'easeInOutQuint');
 		}
 
-		if (datasection==0){
+		if (datasection == 0){
 			offSetKey1();
 		}
-		if (datasection==1){
+		if (datasection == 1){
 			offSetKey1();
 		}
-		if (datasection==2){
+		if (datasection == 2){
 			offSetKey1();
 		}
-		if (datasection==3){
+		if (datasection == 3){
 			offSetKey2();
 		}
-		if (datasection==5){
+		if (datasection == 5){
 			offSetKey1();
 		}
-		if (datasection==6){
+		if (datasection == 6){
 			offSetKey2();
 		}
 	}
@@ -406,43 +355,27 @@ $(function(){
 		datasection = $(this).attr('data-section');
 		goToByScroll(datasection);
 	});
-
-
-	//
-	// $('.bxslider').bxSlider({
-	// 	mode: 'fade',
-	// 	captions: true,
-	// 	adaptiveHeight: true,
-	// 	// pagerCustom: '.bx-pager',
-	// 	onSlideAfter: function(){
-	// 	// do mind-blowing JS stuff here
-	// 	}
-	// });
 	
-	
-	// 修正蓋圖
+	// Video Background
+	$('.player .video-wrapper').css('background','#D8D8D8');
+
+	// Situation_svg - Tips Z-index
 	var $icnTag = $('.icn-tag');
-	$('.icn-tag').css({'z-index':1});
+	$icnTag.css({'z-index':1});
 	$icnTag.hover(function() {
 		$(this).css({'z-index':9999});
 	},function(){
 		$(this).css({'z-index':1});
 	});
-	
-
-	//
-	$('.player .video-wrapper').css('background','#D8D8D8');
-
-	// console
-	// console.log(si);
-
-	// console.log('$s0Top'+':'+$s0Top);
-	// console.log('$s1Top'+':'+$s1Top);
-	// console.log('$s2Top'+':'+$s2Top);
-	// console.log('$s3Top'+':'+$s3Top);
-	// console.log('$s4Top'+':'+$s4Top);
-	// console.log('$s5Top'+':'+$s5Top);
-	// console.log('$s6Top'+':'+$s6Top);
-	// console.log('$menuTop'+':'+$menuTop);
 
 });
+
+/*	----------------------------------------
+*	Console
+*	----------------------------------------
+*/	
+	//	Get Section Offset
+	// $section.each(function(){
+	// 	console.log($section.eq(_a).offset());
+	// 	_a++;
+	// });
